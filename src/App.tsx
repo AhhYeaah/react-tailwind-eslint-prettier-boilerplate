035cc3d56acaf9810343ae9561cd5965ab950f67
AhhYeaah <vivi26743@hotmail.com>
2023-02-01 16:49:25 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function App() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="font-bold text-3xl">Hello world</h1>
+    </div>
+  );
+}
